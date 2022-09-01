a060ab2b2bf8e3f1fc6da48ff60ee74b01e6534a
Konstantin <konstantin.androsov@gmail.com>
2022-09-01 11:50:51 +0200

    Updated environment

--- env.sh
+++ env.sh
@@ -28,27 +28,38 @@ action() {
     # source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_101 x86_64-centos7-gcc8-opt
     # conda activate bbtt
 
-    export SCRAM_ARCH=el8_amd64_gcc10
+    local os_version=$(cat /etc/os-release | grep VERSION_ID | sed -E 's/VERSION_ID="([0-9]+)"/\1/')
+    if [ $os_version = "7" ]; then
+        export SCRAM_ARCH=slc7_amd64_gcc10
+    else
+        export SCRAM_ARCH=el8_amd64_gcc10
+    fi
     CMSSW_VER=CMSSW_12_4_8
-    if ! [ -f soft/$CMSSW_VER/.installed ]; then
-        run_cmd mkdir -p soft
-        run_cmd cd soft
+    if ! [ -f "$this_dir/soft/CentOS$os_version/$CMSSW_VER/.installed" ]; then
+        run_cmd mkdir -p "$this_dir/soft/CentOS$os_version"
+        run_cmd cd "$this_dir/soft/CentOS$os_version"
         if [ -d $CMSSW_VER ]; then
             echo "Removing incomplete $CMSSW_VER installation..."
             run_cmd rm -rf $CMSSW_VER
+            run_cmd rm -f "$this_dir/soft/CentOS$os_version/bin/python"
         fi
-        echo "Creating new $CMSSW_VER area..."
+        echo "Creating $CMSSW_VER area for CentOS$os_version in $PWD ..."
         run_cmd scramv1 project CMSSW $CMSSW_VER
         run_cmd cd $CMSSW_VER/src
         run_cmd eval `scramv1 runtime -sh`
+        run_cmd mkdir -p Framework/NanoProd/
+        run_cmd ln -s "$this_dir/NanoProd" Framework/NanoProd/python
         run_cmd scram b -j8
-        run_cmd cd ../../..
-        touch soft/$CMSSW_VER/.installed
+        run_cmd cd "$this_dir"
+        run_cmd mkdir -p "$this_dir/soft/CentOS$os_version/bin"
+        run_cmd ln -s $(which python3) "$this_dir/soft/CentOS$os_version/bin/python"
+        touch "$this_dir/soft/CentOS$os_version/$CMSSW_VER/.installed"
     else
-        run_cmd cd soft/$CMSSW_VER/src
+        run_cmd cd "$this_dir/soft/CentOS$os_version/$CMSSW_VER/src"
         run_cmd eval `scramv1 runtime -sh`
-        run_cmd cd ../../..
+        run_cmd cd "$this_dir"
     fi
+    export PATH="$this_dir/soft/CentOS$os_version/bin:$PATH"
     source /afs/cern.ch/user/m/mrieger/public/law_sw/setup.sh
     source "$( law completion )"
 }

015a48aca9fd5480c5cdece434582eb73508be95
Konstantin <konstantin.androsov@gmail.com>
2022-09-23 17:35:27 +0200

    Refined gen particle filters and added Zqq

--- NanoProd/customiseNano.py
+++ NanoProd/customiseNano.py
@@ -34,11 +34,10 @@ def customise(process):
   process.MessageLogger.cerr.FwkReport.reportEvery = 100
   process.finalGenParticles.select = cms.vstring(
     "drop *",
-    "keep++ abs(pdgId) == 15 & (pt > 15 ||  isPromptDecayed() )",#  keep full tau decay chain for some taus
-    "keep+ abs(pdgId) == 15 ",  #  keep first gen decay product for all tau
+    "keep++ abs(pdgId) == 15", # keep full decay chain for taus
     "+keep abs(pdgId) == 11 || abs(pdgId) == 13 || abs(pdgId) == 15", #keep leptons, with at most one mother back in the history
+    "+keep+ abs(pdgId) == 6 || abs(pdgId) == 23 || abs(pdgId) == 24 || abs(pdgId) == 25 || abs(pdgId) == 35 || abs(pdgId) == 39  || abs(pdgId) == 9990012 || abs(pdgId) == 9900012",   # keep VIP particles
     "drop abs(pdgId)= 2212 && abs(pz) > 1000", #drop LHC protons accidentally added by previous keeps
-    "keep+ abs(pdgId) == 23 || abs(pdgId) == 24 || abs(pdgId) == 25 || abs(pdgId) == 35 || abs(pdgId) == 39  || abs(pdgId) == 9990012 || abs(pdgId) == 9900012",   # keep VIP particles
   )
 
   process = nanoAOD_addDeepInfoAK4CHS(process, False, False, True)

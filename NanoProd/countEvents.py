7cf220a2a9e02acdfb4014e975b8183e6d8f0459
Jaime Leon <jleonhol@gmail.com>
2022-10-06 17:31:12 +0200

    Fixes and comments

--- NanoProd/countEvents.py
+++ NanoProd/countEvents.py
@@ -1,4 +1,3 @@
-from analysis_tools import Dataset, Process
 import os
 import numpy as np
 import yaml
@@ -29,15 +28,15 @@ def locate_files(folder, dataset_name=None):
                     outputfiles.append(os.path.join(root, file))
     return outputfiles
 
-def chech_files(int_folder, final_folder, sample_file):
+def check_files(int_folder, final_folder, sample_file):
     with open(sample_file) as f:
         samples = yaml.load(f, yaml.Loader)
 
     dataset_names = os.listdir(final_folder)
     for dataset_name in dataset_names:
         miniaod_d = samples[dataset_name]['miniAOD'].split("/")[1]
-        int_files = locate_files(int_folder + miniaod_d, dataset_name)
-        final_files = locate_files(final_folder + dataset_name)
+        int_files = locate_files(os.path.join(int_folder + miniaod_d), dataset_name)
+        final_files = locate_files(os.path.join(final_folder + dataset_name))
 
         df1 = ROOT.RDataFrame("Events", tuple(int_files))
         df2 = ROOT.RDataFrame("Events", tuple(final_files))
@@ -51,19 +50,24 @@ def chech_files(int_folder, final_folder, sample_file):
             sum1 = len(np.unique(columns1['event']))
             sum2 = len(np.unique(columns2['event']))
         else:
-            columns1 = df1.AsNumpy(['event', 'run', 'luminosityBlock'])
-            columns2 = df2.AsNumpy(['event', 'run', 'luminosityBlock'])
-            sum1 = len(np.unique(columns1['event', 'run', 'luminosityBlock'], axis=0))
-            sum2 = len(np.unique(columns2['event', 'run', 'luminosityBlock'], axis=0))
+            columns1 = df1.AsNumpy(columns=['event', 'run', 'luminosityBlock'])
+            columns1 = np.stack((columns1['event'], columns1['run'], columns1['luminosityBlock']), axis=-1)
+            columns2 = df2.AsNumpy(columns=['event', 'run', 'luminosityBlock'])
+            columns2 = np.stack((columns2['event'], columns2['run'], columns2['luminosityBlock']), axis=-1)
+            sum1 = len(np.unique(columns1, axis=0))
+            sum2 = len(np.unique(columns2, axis=0))
 
         print("{:>50} {:>10} {:>10} {:>10} {:>5} {:>5}".format(
             dataset_name, sum1, sum2, sum_count.GetValue(),
             sum1 - sum2, sum2 - sum_count.GetValue()))
 
+
 if __name__ == "__main__":
+
+    assert len(sys.argv) == 4
 
     int_folder = sys.argv[1]
     final_folder = sys.argv[2]
     sample_file = sys.argv[3]
 
-    chech_files(int_folder, final_folder, sample_file)
+    check_files(int_folder, final_folder, sample_file)

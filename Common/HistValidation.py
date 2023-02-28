08e2cb3ea99321337dc63df6a135ffb48c3661aa
Valeria D'Amante <valeriadamante@gmail.com>
2023-02-28 14:33:14 +0100

    applied comments

--- Common/HistValidation.py
+++ Common/HistValidation.py
@@ -26,7 +26,7 @@ for key in keyList:
                 print(f""" key = {key},
                     bin_number = {bin_number}, bin_content = {bin_content},
                     bin_center_X = {hist.GetXaxis().GetBinCenter(x_bin_number)},
-                    bin_center_y = {hist.GetYaxis().GetBinCenter(x_bin_number)}""")
+                    bin_center_y = {hist.GetYaxis().GetBinCenter(y_bin_number)}""")
 prefix = "jet_pt_eta"
 jet_flavors= [0, 4,5]
 wps = ["Loose", "Medium", "Tight"]
@@ -41,12 +41,12 @@ for jet_fl in jet_flavors:
                 bin_number = hist.GetBin(x_bin_number,y_bin_number)
                 num = hist_num.GetBinContent(bin_number)
                 den = hist_den.GetBinContent(bin_number)
-                c_low = ROOT.TEfficiency.ClopperPearson(den, num, 1-0.68, False)
-                c_up = ROOT.TEfficiency.ClopperPearson(den, num, 1-0.68, True)
+                c_low = ROOT.TEfficiency.ClopperPearson(den, num, 0.68, False)
+                c_up = ROOT.TEfficiency.ClopperPearson(den, num, 0.68, True)
                 #print(f"low = {c_low}, up = {c_up}")
                 eff= num/den
                 rel_cl_size=(c_up- c_low)/(2*eff)
-                if round(rel_cl_size,2)>0.01:
+                if rel_cl_size>0.01:
                     print(f""" key_num = {key_num},
                           key_den={key_den},
                           bin_number = {bin_number},
@@ -58,4 +58,3 @@ for jet_fl in jet_flavors:
                           c_up = {c_up},
                           eff = {eff}
                           rel_cl_size={rel_cl_size}""")
-    #i+=1

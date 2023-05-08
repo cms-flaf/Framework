abe87d0c873144f158a001e92bfbc74c47fd8837
Valeria D'Amante <valeriadamante@gmail.com>
2023-05-08 16:06:30 +0200

    bug Fix

--- Common/HHKinFitSelection.py
+++ Common/HHKinFitSelection.py
@@ -24,6 +24,7 @@ def GetKinFitConvergence(df):
     if not initialized:
         raise RuntimeError("HH KinFit selection not initialised!")
     df = df.Define('MT2', 'float(analysis::Calculate_MT2(httCand.leg_p4[0], httCand.leg_p4[1], HbbCandidate.leg_p4[0],HbbCandidate.leg_p4[1], MET_p4))')
+
     df = df.Define("kinFit_result", f"""kin_fit::FitProducer::Fit(HbbCandidate.leg_p4[0],HbbCandidate.leg_p4[1],
                                                        httCand.leg_p4[0], httCand.leg_p4[1],
                                                        MET_p4, MET_covXX, MET_covXY, MET_covYY, Jet_ptRes.at(HbbCandidate.leg_index[0]),
@@ -31,12 +32,18 @@ def GetKinFitConvergence(df):
     df = df.Define('kinFit_convergence', 'kinFit_result.convergence')
     df = df.Define('kinFit_m', 'float(kinFit_result.mass)')
     df = df.Define('kinFit_chi2', 'float(kinFit_result.chi2)')
+    #df = df.Define("first_index", "httCand.leg_index[0]")
+    #df = df.Define("second_index", "httCand.leg_index[1]")
+    #df = df.Define("Tau_DecayMode_size", "Tau_decayMode.size()")
+    #df = df.Define("Tau_pt_size", "Tau_pt.size()")
+    #df.Display({"first_index","second_index","Tau_DecayMode_size","Tau_pt_size"}).Print()
     df = df.Define('SVfit_result',
-                '''sv_fit::FitProducer::Fit(httCand.leg_p4[0], Leg::tau, Tau_decayMode.at(httCand.leg_index[0]),
-                                            httCand.leg_p4[1], Leg::tau, Tau_decayMode.at(httCand.leg_index[1]),
-                                            MET_p4, MET_covXX, MET_covXY, MET_covYY)''')
+                """sv_fit::FitProducer::Fit(httCand.leg_p4[0], httCand.leg_type[0], Tau_decayMode.at(httCand.leg_index[0]),
+                                            httCand.leg_p4[1], httCand.leg_type[1], Tau_decayMode.at(httCand.leg_index[1]),
+                                            MET_p4, MET_covXX, MET_covXY, MET_covYY)""")
 
     df = df.Define('SVfit_valid', 'int(SVfit_result.has_valid_momentum)')
+    '''
     df = df.Define('SVfit_pt', 'float(SVfit_result.momentum.pt())')
     df = df.Define('SVfit_eta', 'float(SVfit_result.momentum.eta())')
     df = df.Define('SVfit_phi', 'float(SVfit_result.momentum.phi())')
@@ -48,6 +55,8 @@ def GetKinFitConvergence(df):
     df = df.Define('SVfit_mt', 'float(SVfit_result.transverseMass)')
     df = df.Define('SVfit_mt_error', 'float(SVfit_result.transverseMass_error)')
     cols_to_append=['MT2','kinFit_result','kinFit_convergence','kinFit_m','kinFit_chi2', 'SVfit_valid', 'SVfit_pt', 'SVfit_eta', 'SVfit_phi', 'SVfit_m', 'SVfit_pt_error', 'SVfit_eta_error', 'SVfit_phi_error', 'SVfit_m_error', 'SVfit_mt', 'SVfit_mt_error']
+    '''
+    cols_to_append=['MT2','kinFit_result','kinFit_convergence','kinFit_m','kinFit_chi2']
     return df,cols_to_append
 
 

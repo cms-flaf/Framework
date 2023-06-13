6eeda6f03bb9340800de56a115f760db633be55b
Valeria D'Amante <valeriadamante@gmail.com>
2023-06-13 16:28:05 +0200

    bug fix

--- Analysis/SkimProducer.py
+++ Analysis/SkimProducer.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
   parser.add_argument('--inputDir', required=True, type=str)
   parser.add_argument('--outputDir', required=True, type=str)
   parser.add_argument('--centralFile', required=False, type=str, default='Events.root')
-  parser.add_argument('--recreateOutDir', required=False, type=bool, default=False)
+  parser.add_argument('--recreateOutDir', required=False, type=bool, default=True)
+  parser.add_argument('--test', required=False, type=bool, default=False)
   args = parser.parse_args()
   all_files = os.listdir(args.inputDir)
   central_idx = all_files.index(args.centralFile)
@@ -29,20 +30,22 @@ if __name__ == "__main__":
   syst_trees = []
   k=0
   for systFile in all_files:
-    if k>2 : continue
+    if args.test and k>2 : continue
     k+=1
     outFileName = os.path.join(args.outputDir, systFile.strip('.root'))
     inFileCentralName = os.path.join(args.inputDir, args.centralFile)
     inFileShiftedName = os.path.join(args.inputDir, systFile)
     treeName = systFile.strip('.root')
     cmd = f"""python3 /afs/cern.ch/work/v/vdamante/hhbbTauTauRes/prod/Framework/Analysis/SkimEvents.py --inFileCentral {inFileCentralName} --inFileShifted {inFileShiftedName} --outFile {outFileName} --treeName {treeName}"""
-    print(cmd)
     sh_call(cmd, True)
-  print(k)
   for file_syst in os.listdir(args.outputDir):
     syst_files_to_merge.append(f'{args.outputDir}/{file_syst}')
-  print(syst_files_to_merge)
   finalFile = os.path.join(args.outputDir, args.centralFile)
-  hadd_str = f'hadd {finalFile} '
+  if os.path.exists(finalFile):
+    os.remove(finalFile)
+  hadd_str = f'hadd -k -fk {finalFile} '
   hadd_str += ' '.join(f for f in syst_files_to_merge)
-  sh_call(hadd_str, True)
+  print(hadd_str)
+  sh_call([hadd_str], True)
+  for syst_file in syst_files_to_merge:
+    os.remove(syst_file)

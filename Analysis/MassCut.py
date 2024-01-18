7c4db802e4495bb8c5e65652b17f97193fc693a0
Valeria D'Amante <valeriadamante@gmail.com>
2024-01-18 12:02:17 +0100

    MassCut Limits fix

--- Analysis/MassCut.py
+++ Analysis/MassCut.py
@@ -3,6 +3,9 @@ import os
 import sys
 import yaml
 import numpy as np
+import matplotlib; import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
 if __name__ == "__main__":
     sys.path.append(os.environ['ANALYSIS_PATH'])
 
@@ -16,30 +19,6 @@ inFiles = Utilities.ListToVector(["/eos/home-k/kandroso/cms-hh-bbtautau/anaTuple
 df_initial = ROOT.RDataFrame("Events", inFiles)
 
 df_initial = df_initial.Define("nSelBtag", f"int(b1_idbtagDeepFlavB >=2) + int(b2_idbtagDeepFlavB >=2)")
-
-'''
-#print(df.Count().GetValue())
-df_boosted = df_initial.Define("SelectedFatJet_size","SelectedFatJet_pt.size()").Filter("SelectedFatJet_size>0")
-#print(df.Count().GetValue())
-
-df_boosted = df_boosted.Define("FatJet_atLeast1BHadron","SelectedFatJet_nBHadrons>0").Filter("SelectedFatJet_pt[FatJet_atLeast1BHadron].size()==1")
-#print(df.Count().GetValue())
-
-df_boosted= df_boosted.Define("SelectedFatJet_pNetmass","SelectedFatJet_particleNet_mass[FatJet_atLeast1BHadron][0]")
-df_boosted= df_boosted.Define("SelectedFatJet_pt1had","SelectedFatJet_pt[FatJet_atLeast1BHadron][0]")
-np_dict_boosted = df_boosted.AsNumpy(["SelectedFatJet_pNetmass","SelectedFatJet_pt1had"])
-np_array_mass_boosted = np_dict_boosted["SelectedFatJet_pNetmass"]
-np_array_pt_boosted = np_dict_boosted["SelectedFatJet_pt1had"]
-#print(len(np_array))
-print("quantile max for pt boosted = ", np.quantile(np_array_pt_boosted, 1-0.005))
-print("quantile min for pt boosted = ",np.quantile(np_array_pt_boosted, 0.005))
-print("quantile max for mass boosted = ", np.quantile(np_array_mass_boosted, 1-0.005))
-print("quantile min for mass boosted = ",np.quantile(np_array_mass_boosted, 0.005))
-
-
-#df_resolved = df_initial.Define("is_resolved","SelectedFatJet_pt.size()").Filter("SelectedFatJet_size==0 && b1_pt >0 && b2_pt>0 && b2_hadronFlavour==5 && b2_hadronFlavour==5")
-'''
-
 df_resolved = df_initial.Filter(f"b1_pt >0 && b2_pt>0 && b1_hadronFlavour==5 && b2_hadronFlavour==5 && tau2_idDeepTau2017v2p1VSjet >= {Utilities.WorkingPointsTauVSjet.Medium.value} && nSelBtag >1")
 for idx in [0,1]:
     df_resolved = defineP4(df_resolved, f"tau{idx+1}")
@@ -63,49 +42,12 @@ with open(histcfg, 'r') as f:
 x_bins = hist_cfg_dict['bb_m_vis']['x_bins']
 y_bins = hist_cfg_dict['tautau_m_vis']['x_bins']
 
-def getModel(x_bins,y_bins):
-    if type(x_bins)==list :
-        x_bins_vec = Utilities.ListToVector(x_bins, "double")
-        if type(y_bins)==list:
-            y_bins_vec = Utilities.ListToVector(y_bins, "double")
-            return ROOT.RDF.TH2DModel("", "", x_bins_vec.size()-1, x_bins_vec.data(), y_bins_vec.size()-1, y_bins_vec.data())
-        else:
-            n_y_bins, y_bin_range = y_bins.split('|')
-            start_y,stop_y = y_bin_range.split(':')
-            return ROOT.RDF.TH2DModel("", "",x_bins_vec.size()-1, x_bins_vec.data(), int(n_y_bins), float(start_y), float(stop_y))
-    else:
-        n_x_bins, x_bin_range = x_bins.split('|')
-        start_x,stop_x = x_bin_range.split(':')
-        if type(y_bins)==list:
-            y_bins_vec = Utilities.ListToVector(y_bins, "double")
-            return ROOT.RDF.TH2DModel("", "",  int(n_x_bins), float(start_x), float(stop_x), y_bins_vec.size()-1, y_bins_vec.data())
-        else:
-            n_y_bins, y_bin_range = y_bins.split('|')
-            start_y,stop_y = y_bin_range.split(':')
-            return ROOT.RDF.TH2DModel("", "", int(n_x_bins), float(start_x), float(stop_x), int(n_y_bins), float(start_y), float(stop_y))
-
-hist2D = df_resolved.Histo2D(getModel(x_bins,y_bins), 'bb_m_vis', 'tautau_m_vis')
-c1 = ROOT.TCanvas("600","800")
-hist2D.Draw()
-c1.SaveAs('prova.png')
-#myline=TLine(0,10,0,100)
-#myline.Draw("same")
-
-import matplotlib; import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import mplhep as hep
 plt.style.use(hep.style.ROOT)
-
-#hep.cms.text('Preliminary', fontsize=40)
-#hep.cms.lumitext((chn_unicodes[self.channel] + " (" + cat_map[self.category] + ") | " +
-#                          self.sample + " (" + self.year + ")"),
-#                         fontsize=24) # r"138 $fb^{-1}$ (13 TeV)
-
 ylabel = r"$m_{{\tau\tau}}^{{vis}}$  [GeV]"
 xlabel = r"$m_{{bb}}^{{vis}}$  [GeV]"
 #cmin=10,
 plt.hist2d(np_array_mass_bb_resolved, np_array_mass_tt_resolved, bins=(100, 100), range = [[0, 300.],[0,300.]], cmap=plt.cm.jet)
-#plt.plot([20., 120., 120., 20., 20.], [50., 50., 280., 280., 50.], c='red', linewidth=10, label=r"Mass window cut")
+
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 
@@ -117,6 +59,4 @@ y_p = np.append(y_p, y_p[0])
 plt.plot(x_p,y_p, c='red')
 
 plt.show()
-plt.savefig('pyprova.png')
-#plt.hist2d(np_xarray_mass_bb_resolved, np_array_mass_tt_resolved, bins=(300, 30), cmap=plt.cm.jet)
-#plt.show()
+plt.savefig('MassCut_limits.png')

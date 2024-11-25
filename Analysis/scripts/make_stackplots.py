0b383f5e94fa8904e6fc1bac2af4bf023159a63f
Devin <aebid@tamu.edu>
2024-11-25 14:22:34 +0100

    Changed formatting for HistPlotter input files

--- Analysis/scripts/make_stackplots.py
+++ Analysis/scripts/make_stackplots.py
@@ -1,19 +1,20 @@
 import os
 
 
-indir = "/eos/user/d/daebi/ANA_FOLDER_DEV/histograms/Run32022_13Nov24_NoMuonSF/Run3_2022/merged/"
+indir = "/eos/user/d/daebi/ANA_FOLDER_DEV/histograms/Run32022_13Nov24/Run3_2022/merged/"
 
 varnames = ["lep1_pt", "lep1_eta", "lep2_pt", "lep2_eta", "bjet1_btagPNetB", "bjet2_btagPNetB"]
-varnames = ["lep1_pt", "lep1_eta", "lep2_pt", "lep2_eta"]
+varnames = ["lep1_pt"]
 channellist = ["e", "eE", "eMu", "mu", "muMu"]
 channellist = ["mu", "muMu"]
 
+era = "Run3_2022"
 plotdir = "plots_19Nov_NoMuonSF/"
 
 for var in varnames:
     for channel in channellist:
-        filename = os.path.join(indir, var, f"tmp/all_histograms_{var}_Central.root")
+        filename = os.path.join(indir, var, f"{var}.root")
         print("Loading fname ", filename)
         os.makedirs(plotdir, exist_ok=True)
         outname = os.path.join(plotdir, f"HHbbWW_{channel}_{var}_StackPlot.pdf")
-        os.system(f"python3 HistPlotter.py --inFile {filename} --bckgConfig ../config/HH_bbWW/background_samples.yaml --globalConfig ../config/HH_bbWW/global.yaml --sampleConfig ../config/Run3_2022/samples.yaml --outFile {outname} --var {var} --category inclusive --channel {channel} --uncSource Central --wantData True --year Run3_2022 --wantQCD False --rebin False --analysis HH_bbWW")
+        os.system(f"python3 ../HistPlotter.py --inFile {filename} --bckgConfig ../../config/HH_bbWW/background_samples.yaml --globalConfig ../../config/HH_bbWW/global.yaml --outFile {outname} --var {var} --category inclusive --channel {channel} --uncSource Central --wantData True --year Run3_2022 --wantQCD False --rebin False --analysis HH_bbWW --qcdregion OS_Iso --sigConfig ../../config/HH_bbWW/{era}/samples.yaml")

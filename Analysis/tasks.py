ec6b1d703f15b891ca1f7b285c7243f940e65d54
Devin Aebi <45836403+aebid@users.noreply.github.com>
2025-02-19 13:46:26 +0100

    Add AnalysisCacheTask for bbWW (#95)
    * Started new tasks for the AnalysisCache (better name coming). Cache to create high level variables
    
    * Fixed some variable type and name issues
    
    * Changed the arg bools to action store_true since argparse doesn't like bools. Restructured how the signal names are handled for plot making
    
    * AnaTuples now handle channelId so we do not need to redefine
    
    * Added the bbWW AnalysisCache task, bbtautau still uses old AnaCacheTupleTask in AnaProd
    
    * Added DNN histograms
    
    * Removed useless prints
    
    * Python file for the AnalysisCache task for bbWW
    
    * Added the DNN model into the config folder, this is early and later we will include a documentation yaml for the inputs/training and such
    
    * Updated DNN usage to now use v2 model (including jet objs)
    
    * Undo changes on bbtautau plotting configs
    
    * Undo simple changes
    
    * Reset merge sample types to full list

--- Analysis/tasks.py
+++ Analysis/tasks.py
@@ -3,11 +3,13 @@ import os
 import yaml
 import contextlib
 import luigi
+import threading
+
 
 from RunKit.run_tools import ps_call
 from RunKit.crabLaw import cond as kInit_cond, update_kinit_thread
 from run_tools.law_customizations import Task, HTCondorWorkflow, copy_param,get_param_value
-from AnaProd.tasks import AnaTupleTask, DataMergeTask, AnaCacheTupleTask, DataCacheMergeTask
+from AnaProd.tasks import AnaTupleTask, DataMergeTask, AnaCacheTupleTask, DataCacheMergeTask, AnaCacheTask
 
 
 unc_cfg_dict = None
@@ -362,4 +364,113 @@ class MergeTask(Task, HTCondorWorkflow, law.LocalWorkflow):
             with tmp_outFile.localize("r") as tmpFile, self.output().localize("w") as outFile:
                 RenameHistsProducer_cmd = ['python3', RenameHistsProducer,'--inFile', tmpFile.path, '--outFile', outFile.path, '--var', var, '--year', getYear(self.period), '--ana_path', self.ana_path(), '--period', self.period]
                 ps_call(RenameHistsProducer_cmd,verbose=1)
+
+
+
+
+
+class AnalysisCacheTask(Task, HTCondorWorkflow, law.LocalWorkflow):
+    max_runtime = copy_param(HTCondorWorkflow.max_runtime, 30.0)
+    n_cpus = copy_param(HTCondorWorkflow.n_cpus, 1)
+
+    def workflow_requires(self):
+        workflow_dict = {}
+        workflow_dict["anaTuple"] = {
+            br_idx: AnaTupleTask.req(self, branch=br_idx)
+            for br_idx, _ in self.branch_map.items()
+        }
+        return workflow_dict
+
+    def requires(self):
+        return [ AnaTupleTask.req(self, max_runtime=AnaTupleTask.max_runtime._default) ]
+
+    def create_branch_map(self):
+        branches = {}
+        anaProd_branch_map = AnaTupleTask.req(self, branch=-1, branches=()).branch_map
+        for br_idx, (sample_id, sample_name, sample_type, input_file) in anaProd_branch_map.items():
+            branches[br_idx] = (sample_name, sample_type)
+        return branches
+
+    def output(self):
+        sample_name, sample_type = self.branch_data
+        outFileName = os.path.basename(self.input()[0].path)
+        outDir = os.path.join('anaCacheTuples', self.period, sample_name, self.version)
+        finalFile = os.path.join(outDir, outFileName)
+        return self.remote_target(finalFile, fs=self.fs_anaCacheTuple)
+
+    def run(self):
+        #For now, this is only for bbWW, for the bbtautau we still use the AnaCahceTupleTask found in AanProd folder
+        sample_name, sample_type = self.branch_data
+        unc_config = os.path.join(self.ana_path(), 'config',self.period, f'weights.yaml')
+        producer_anacachetuples = os.path.join(self.ana_path(), 'Analysis', 'hh_bbWW_AnaCacheProducer.py')
+        global_config = os.path.join(self.ana_path(), self.global_params['analysis_config_area'], f'global.yaml')
+        thread = threading.Thread(target=update_kinit_thread)
+        thread.start()
+        try:
+            job_home, remove_job_home = self.law_job_home()
+            input_file = self.input()[0]
+            print(f"considering sample {sample_name}, {sample_type} and file {input_file.path}")
+            customisation_dict = getCustomisationSplit(self.customisations)
+            deepTauVersion = customisation_dict['deepTauVersion'] if 'deepTauVersion' in customisation_dict.keys() else ""
+            with input_file.localize("r") as local_input, self.output().localize("w") as outFile:
+                anaCacheTupleProducer_cmd = ['python3', producer_anacachetuples,'--inFileName', local_input.path, '--outFileName', outFile.path,  '--uncConfig', unc_config, '--globalConfig', global_config]
+                if self.global_params['store_noncentral'] and sample_type != 'data':
+                    anaCacheTupleProducer_cmd.extend(['--compute_unc_variations', 'True'])
+                if deepTauVersion!="":
+                    anaCacheTupleProducer_cmd.extend([ '--deepTauVersion', deepTauVersion])
+                useDNNModel = "HH_bbWW" in self.global_params['analysis_config_area'] #Now bbtautau won't use this DNN model arg (even though this task is only for bbWW right now)
+                if useDNNModel:
+                    dnnFolder = os.path.join(self.ana_path(), self.global_params['analysis_config_area'], 'DNN', 'v2') #'ResHH_Classifier.keras')
+                    anaCacheTupleProducer_cmd.extend([ '--dnnFolder', dnnFolder])
+                ps_call(anaCacheTupleProducer_cmd, verbose=1)
+            print(f"finished to produce anacachetuple")
+
+        finally:
+            kInit_cond.acquire()
+            kInit_cond.notify_all()
+            kInit_cond.release()
+            thread.join()
+
+
+
+
+
+class AnalysisCacheMergeTask(Task, HTCondorWorkflow, law.LocalWorkflow):
+    max_runtime = copy_param(HTCondorWorkflow.max_runtime, 5.0)
+
+    def workflow_requires(self):
+        workflow_dep = {}
+        for idx, prod_branches in self.branch_map.items():
+            workflow_dep[idx] = AnalysisCacheTask.req(self, branches=prod_branches)
+        return workflow_dep
+
+    def requires(self):
+        prod_branches = self.branch_data
+        deps = [ AnalysisCacheTask.req(self, max_runtime=AnaCacheTask.max_runtime._default, branch=prod_br) for prod_br in prod_branches ]
+        return deps
+
+    def create_branch_map(self):
+        anaProd_branch_map = AnalysisCacheTask.req(self, branch=-1, branches=()).branch_map
+        prod_branches = []
+        for prod_br, (sample_name, sample_type) in anaProd_branch_map.items():
+            if sample_type == "data":
+                prod_branches.append(prod_br)
+        return { 0: prod_branches }
+
+    def output(self, force_pre_output=False):
+        outFileName = 'nanoHTT_0.root'
+        output_path = os.path.join('anaCacheTuple', self.period, 'data',self.version, outFileName)
+        return self.remote_target(output_path, fs=self.fs_anaCacheTuple)
+
+    def run(self):
+        producer_dataMerge = os.path.join(self.ana_path(), 'AnaProd', 'MergeNtuples.py')
+        with contextlib.ExitStack() as stack:
+            local_inputs = [stack.enter_context(inp.localize('r')).path for inp in self.input()]
+            with self.output().localize("w") as tmp_local_file:
+                tmpFile = tmp_local_file.path
+                dataMerge_cmd = [ 'python3', producer_dataMerge, '--outFile', tmpFile]
+                dataMerge_cmd.extend(local_inputs)
+                #print(dataMerge_cmd)
+                ps_call(dataMerge_cmd,verbose=1)
+
 

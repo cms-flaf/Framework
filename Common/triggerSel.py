b170f12d0f2cc6366b88363280ddf93c0c4549a4
acagnotta <antimo.cagnotta@cern.ch>
2025-04-15 14:04:02 +0200

    removing useless comments

--- Common/triggerSel.py
+++ Common/triggerSel.py
@@ -35,22 +35,16 @@ class Triggers():
                     df = df.Define(var_name_online, ' && '.join(cut_vars))
                     matching_var = f'{obj}_Matching_{leg_id+1}_{path}'
                     df = df.Define(matching_var, f"""FindMatching( {var_name_offline}, {var_name_online}, {obj}_p4, TrigObj_p4, {self.deltaR_matching} )""")
-                    # if (obj=="ExtraJet"):df.Display([f"{matching_var}", f"{var_name_offline}", f"{var_name_online}", f"{obj}_pt"],10).Print()
-                    # fai tornare un solo indice per ExtraJet se c'è un match
-                    # df = df.Redefine(matching_var, f"Any({matching_var}> -1)")
             for obj in offline_legs:
                 matching_var_bool = f'{obj}_HasMatching_{path}'
                 matching_var_bool_str = " || ".join(f"({obj}_Matching_{i+1}_{path} > -1)" for i in range(len(path_dict['legs'])))
-                # if (obj=="ExtraJet"): print(f"matching_var_bool_str: {matching_var_bool_str}")
                 df = df.Define(matching_var_bool, f'({matching_var_bool_str})')
-                # if (obj=="ExtraJet"):df.Display(matching_var_bool,10).Print()
             df = df.Define(f"HasOOMatching_{path}",  " || ".join(f'({obj}_HasMatching_{path})' for obj in offline_legs))
             fullPathSelection = f'{or_paths} &&  HasOOMatching_{path}'
             fullPathSelection += ' && '.join(additional_conditions)
             hltBranch = f'HLT_{path}'
             hltBranches.append(hltBranch)
             df = df.Define(hltBranch, fullPathSelection)
-            # df.Display([f"HasOOMatching_{path}", f"{hltBranch}"]).Print()
         total_or_string = ' || '.join(hltBranches)
         if applyTriggers:
             df = df.Filter(total_or_string, "trigger application")
